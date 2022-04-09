c86ced133818f91f130dc12caf3c74d56265f710
fujinshuai <jinshuai.fu@shanbay.com>
2022-04-09 21:22:48 +0800

    第5节

--- src/5.ts
+++ src/5.ts
@@ -20,3 +20,78 @@ type GetLast<Arr extends unknown[]> = Arr extends [...unknown[], infer Last]
   : never;
 
 type last = GetLast<arr>;
+
+// PopArr
+
+type GetPopArr<Arr extends unknown[]> = Arr extends [...infer Rest, unknown]
+  ? Rest
+  : never;
+
+type popArr = GetPopArr<arr>;
+
+//×Ö·û´®ÀàÐÍ
+
+type StartWith<
+  Str extends string,
+  Prefix extends string
+> = Str extends `${Prefix}${string}` ? true : false;
+
+type str = "guang and dong";
+
+type StartWithRes = StartWith<str, "guang">;
+
+// Trim
+type TrimStrRight<Str extends string> = Str extends `${infer Rest}${
+  | " "
+  | "\n"
+  | "\t"}`
+  ? TrimStrRight<Rest>
+  : Str;
+type TrimStrRightRes = TrimStrRight<"guang    ">;
+
+type TrimStrLeft<Str extends string> = Str extends `${
+  | " "
+  | "\n"
+  | "\t"}${infer Rest}`
+  ? TrimStrLeft<Rest>
+  : Str;
+
+type TrimStr<Str extends string> = TrimStrRight<TrimStrLeft<Str>>;
+
+// Function
+
+type GetParameters<Func extends Function> = Func extends (
+  ...args: infer Args
+) => unknown
+  ? Args
+  : never;
+
+type GetReturnType<Func extends Function> = Func extends (
+  ...args: unknown[]
+) => infer Return
+  ? Return
+  : never;
+type ReturnRes = GetReturnType<() => "guang">;
+
+// Constructor
+interface Person {
+  name: string;
+}
+
+interface PersonConstructor {
+  new (name: string): Person;
+}
+
+type GetInstanceType<ConstructorType extends new (...args: any) => any> =
+  ConstructorType extends new (...args: any) => infer InstanceType
+    ? InstanceType
+    : any;
+
+type GetInstanceTypeRes = GetInstanceType<PersonConstructor>;
+
+// keyof
+type GetRefProps<Props> = "ref" extends keyof Props
+  ? Props extends { ref?: infer Value | undefined }
+    ? Value
+    : never
+  : never;
